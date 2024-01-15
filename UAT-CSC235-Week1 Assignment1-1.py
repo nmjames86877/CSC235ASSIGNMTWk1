479fe0d0e59d61b6a2ccea9d44ca98734961b0cb
Nicholas M James <147876915+nmjames86877@users.noreply.github.com>
2024-01-14 21:59:27 -0700

    Add files via upload

--- UAT-CSC235-Week1 Assignment1-1.py
+++ UAT-CSC235-Week1 Assignment1-1.py
@@ -1,45 +1,59 @@
-CSC235 Week 1 Assignment 1.1 Nicholas M James
+#CSC235 Week 1 Assignment 1.1 Nicholas M James
 
 # For this assignment we create a tutorial about Python features this code needs to have:
-	# 2 variables
+	# 2 variablesY
 	# input and output
 	# lots of code comments
 
-# print out information about the print command
-	print("***Ribbet Welcome to Python Because it is an Awesome Tutorial Ribbet***")
-	print('In this lesson, we master the "print" Python command.')
-
-# Create 2 variables and Gather user input
+# Print out information about the print command
+print("***Ribbet Welcome to Python Because it is an Awesome Tutorial Ribbet***")
+print('In this lesson, we master the "print" Python command.')
 
 # Create 1st variable
-	readyYN = input("Are you ready to hop in? (Y/N)")
-# note to self later for adding if/than" responses for this variable
+readyYN = input("Are you ready to hop in? (Y/N)")
 
 # Display user input
-	print('Answer is: ', readyYN)
+print('Answer is: ', readyYN)
 
-# Print some more infomration about Python
-	print('How do you like the Python "print" command?')
-	print('Ókay, times up!')
-	print('Love it right, I know I know lol')
-	print('-----------------------------------------------')
-	print('Here is the syntax for the Python "print" command')
-	print('print("Text to display on the screen.")')
-	print(
-	print('How you use the "print"command in Python to print the phrase "I am the HAL 9000 Computer aka FrogPrince aka Nikolo"')
-	print('print("I am the HAL 9000 Computer aka FrogPrince aka Nikolo")')
-	print('The following is displayed in the application')
-	print('I am the HAL 9000 Computer aka FrogPrince aka Nikolo')
-	print('Pro Tip: the print command must be typed in all lowercase.')
-	print('-----------------------------------------------')
+# Print more information about Python
+print('------------------------------------------------')
+print('Sweeeet, Ribbet Ribbet')
+print('------------------------------------------------')
+print('Here is the syntax for the Python "print" command')
+print('print("Text to display on the screen.")')
+print('------------------------------------------------')
+print('Here is an example using the "print" command in Python to print the phrase "I am the HAL 9000 Computer aka FrogPrince aka Nikolo"')
+print('------------------------------------------------')
+print('print("I am the HAL 9000 Computer aka FrogPrince aka Nikolo")')
+print('And the following is displayed in the application')
+print('------------------------------------------------')
+print('I am the HAL 9000 Computer aka FrogPrince aka Nikolo')
+print('------------------------------------------------')
+print('Pro Tip: the print command must be typed in all lowercase.')
+print('-----------------------------------------------')
+
+# Create extra variable
+print('So...Ribbet, How do you like the Python "print" command?')
+feelings = input("Love It or Hate It? (Love It/Hate It)")
+
+# Display extra user input
+print('Feelings are: ', feelings)
+
+# Print some extra information about Python
+print('Love it right?! Ribbet Ribbet,  I know I know lol')
+print('-----------------------------------------------')
 
 # Create 2nd variable
-	rating = input("Please rate this tutorial on a 1-10 scale (with 10 being the best): ")
+rating = input("Please rate this tutorial on a 1-10 scale (with 10 being the best): ")
 
 # Display user input
-	print('Rating of this tutorial receieved a: ', rating)
+print('Rating: ', rating)
 
 # Print some more information about Python
-	print('Tutorial Complete...Great Job!')
-	print('Treat Yourself By Doing Something You Love')
-	print('Until Next Time, Au Revoir')
+print('-----------------------------------------------')
+print('Awesome!!')
+print('-----------------------------------------------')
+print('Tutorial Complete...Great Job! Ribbet')
+print('-----------------------------------------------')
+print('Now, Go And Treat Yourself By Doing Something You Love!')
+print('Until Next Time, Ribbet Ribbet Au Revoir!!!')
